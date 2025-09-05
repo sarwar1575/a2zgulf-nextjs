528868ab6069357b35d86c8874e05e5d62f15106
sarwar <sarwarghulame@triline.co.in>
2025-09-05 15:05:21 +0530

    initial changes

---
+++ src/components/home/BusinessNeeds.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+function BusinessNeeds() {
+  return (
+    <>BusinessNeeds</>
+  )
+}
+
+export default BusinessNeeds;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './Banner'
+import BusinessNeeds from './BusinessNeeds'
 
 export default function Home() {
   return (
     <>
     <Banner />
+    <BusinessNeeds />
     </>
   )
 }
